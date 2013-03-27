611e260e6a6f1f69537129e4e70412f790adace6
Caleb Cauthon <calebcauthon@gmail.com>
2013-03-26 21:58:38 -0500

    initial commit for refinery site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LuslEy::Application.config.secret_token = '5b275b016e61b4260008b611fbefa77edf38af6a55152cdece9c47239e8f1d0f6949be58d53451313c3c4129b5bcf7dcbc63a917e4186ef1a56813b531f1ca33'
